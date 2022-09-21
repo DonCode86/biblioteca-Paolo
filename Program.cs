04461928040a6a21debe431107c9c09fd16999cb
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-21 10:14:46 +0200

    ricerca-per-tipo

--- Program.cs
+++ Program.cs
@@ -2,35 +2,88 @@
 using System.ComponentModel;
 Console.WriteLine("biblioteca Ciccarelli");
 
-List<Documento> documenti = new List<Documento>();
-
-List<Libro> libri = new List<Libro>();
-
-List<Dvd> dvd = new List<Dvd>();
-
-libri.Add(new Libro("libro",100));
-libri.Add(new Libro("bello",200));
-libri.Add(new Libro("mare",10));
 
 
-dvd.Add(new Dvd("matrix",120));
-dvd.Add(new Dvd("matrix 2",100));
-dvd.Add(new Dvd("matrix 3", 80));
+//List<Libro> libri = new List<Libro>();
 
-string titoloUtente = "ma";  
+//List<Dvd> dvd = new List<Dvd>();
 
-foreach(Libro libro in libri)
+//libri.Add(new Libro("libro",100));
+//libri.Add(new Libro("bello",200));
+//libri.Add(new Libro("mare",10));
+
+
+//dvd.Add(new Dvd("matrix",120));
+//dvd.Add(new Dvd("matrix 2",100));
+//dvd.Add(new Dvd("matrix 3", 80));
+
+Biblioteca biblio = new Biblioteca();
+
+List<Dvd> ricerca = biblio.RicercaFilm ("ma");
+foreach (Dvd dvd in ricerca)
 {
-    if(libro.Titolo.Contains(titoloUtente))
-    {
-        Console.WriteLine(libro.Titolo);
-    }
+    Console.WriteLine(dvd.Titolo);
 }
 
-foreach (Dvd DVD in dvd)
+//foreach(Libro libro in libri)
+//{
+//    if(libro.Titolo.Contains(titoloUtente))
+//    {
+//        Console.WriteLine(libro.Titolo);
+//    }
+//}
+
+//foreach (Dvd DVD in dvd)
+//{
+//    if (DVD.Titolo.Contains(titoloUtente))
+//    {
+//        Console.WriteLine(DVD.Titolo);
+//    }
+//}
+
+
+
+public class Biblioteca
 {
-    if (DVD.Titolo.Contains(titoloUtente))
+    List<Documento> documenti = new List<Documento>();
+
+    public Biblioteca()
     {
-        Console.WriteLine(DVD.Titolo);
+        documenti.Add(new Libro("libro", 100));
+        documenti.Add(new Libro("bello", 200));
+        documenti.Add(new Libro("mare", 10));
+
+
+        documenti.Add(new Dvd("matrix", 120));
+        documenti.Add(new Dvd("matrix 2", 100));
+        documenti.Add(new Dvd("matrix 3", 80));
+    }
+
+    public List<Documento> RicercaDocumenti(string query)
+    {
+        List<Documento> res = new List<Documento>();
+        foreach (Documento doc in documenti)
+        {
+            if (doc.Titolo.Contains(query))
+            {
+               res.Add(doc);
+            }
+        }
+
+        return res;
+    }
+
+    public List<Dvd> RicercaFilm(string query)
+    {
+        List<Dvd> res = new List<Dvd>();
+        foreach (Documento doc in documenti)
+        {
+            if (doc.GetType() == typeof(Dvd) && doc.Titolo.Contains(query))
+            {
+                res.Add((Dvd)doc);
+            }
+        }
+
+        return res;
     }
 }
